dd61b64e671e16e6581ac0278561267654b18562
Hoa <huynhlequang@gmail.com>
2022-02-04 13:37:20 +0900

    stdafx.h: add template function string_format (02/04/2022 - 13:37)

--- FFMPegModule/stdafx.h
+++ FFMPegModule/stdafx.h
@@ -47,6 +47,16 @@ using namespace cv::dnn;
 using namespace std;
 using namespace dnn;
 
-
+template<typename ... Args>
+std::string string_format(const std::string& format, Args ... args)
+{
+    size_t size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0' 
+    if (size <= 0) {
+        throw std::runtime_error("Error during formatting.");
+    }
+    std::unique_ptr<char[]> buf(new char[size]);
+    snprintf(buf.get(), size, format.c_str(), args ...);
+    return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside }
+}
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
 #pragma warning(disable:4819)
