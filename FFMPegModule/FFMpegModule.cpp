29737717db8d169e745d089cf2e44401a3898574
Hoa <huynhlequang@gmail.com>
2022-02-15 16:35:05 +0900

    FFMpegModule.cpp: replace function name from UpdateStatus to SetUpdateStatusCallback (02/15/2022 - 16:34)

--- FFMPegModule/FFMpegModule.cpp
+++ FFMPegModule/FFMpegModule.cpp
@@ -35,7 +35,7 @@ DLLEXPORT void Release()
 	}
 }
 
-DLLEXPORT void UpdateStatus(void (__stdcall* callback)(int64_t hour, int64_t minute, int64_t second))
+DLLEXPORT void SetUpdateStatusCallback(void (__stdcall* callback)(int64_t hour, int64_t minute, int64_t second))
 {
 	if (_avCapture)
 		_avCapture->_callback = callback;
