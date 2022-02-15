9db45be496f6e959d5be5580af79b3fa434a4ac1
Hoa <huynhlequang@gmail.com>
2022-02-15 16:34:44 +0900

    FFMpegModule.h: replace function name from UpdateStatus to SetUpdateStatusCallback (02/15/2022 - 16:34)

--- FFMPegModule/FFMpegModule.h
+++ FFMPegModule/FFMpegModule.h
@@ -6,4 +6,4 @@
 DLLEXPORT bool IsRun();
 DLLEXPORT int Process(const char* strInputFile, const char* strOutputFile, long bitrate, float fps, long duration);
 DLLEXPORT void Release();
-DLLEXPORT void UpdateStatus(void (__stdcall * callback)(int64_t hour, int64_t minute, int64_t second));
+DLLEXPORT void SetUpdateStatusCallback(void (__stdcall * callback)(int64_t hour, int64_t minute, int64_t second));
